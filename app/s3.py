692ce6b5be22df2955e670f1ef98e83f85b2fef4
Gábor Várkonyi <vg4b0r@gmail.com>
2026-02-28 18:30:56 +0100

    Fixed healtcheck and s3 config

--- app/s3.py
+++ app/s3.py
@@ -3,7 +3,12 @@ import boto3
 from botocore.client import Config
 
 def s3_client():
-    return boto3.client("s3", region_name=os.environ.get("AWS_REGION"))
+    region = os.environ.get("AWS_REGION") or os.environ.get("AWS_DEFAULT_REGION") or "eu-north-1"
+    return boto3.client(
+        "s3",
+        region_name=region,
+        config=Config(signature_version="s3v4")
+    )
 
 def upload_fileobj(fileobj, bucket: str, key: str, content_type: str):
     s3 = s3_client()
@@ -18,7 +23,7 @@ def delete_object(bucket: str, key: str):
     s3 = s3_client()
     s3.delete_object(Bucket=bucket, Key=key)
 
-def presigned_get_url(bucket: str, key: str, expires_sec: int = 300) -> str:
+def presigned_get_url(bucket: str, key: str, expires_sec: int = 3600) -> str:
     s3 = s3_client()
     return s3.generate_presigned_url(
         "get_object",
